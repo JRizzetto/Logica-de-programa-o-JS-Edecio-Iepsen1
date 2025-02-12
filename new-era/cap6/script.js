98038042d7fc8581f97c9a6c744152e47bf7ae1b
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-02-12 17:24:35 -0300

    Exercício 6 feito

--- new-era/cap6/script.js
+++ new-era/cap6/script.js
@@ -2,23 +2,49 @@
 // em exposição em uma tv do consultório.
 
 const form = document.querySelector("form");
-const btUrgencia = document.getElementById("btUrgencia");
-const btAtender = document.getElementById("btAtender");
 const textAtendimento = document.getElementById("textAtendimento");
 const textResp = document.getElementById("textResp");
-
 
 const arrayInput = []
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    let inputName = form.pacienteInput.value
+    let inputName = form.pacienteInput.value;
     arrayInput.push(inputName);
+    let lista = "";
+
+    for(let i = 0; i < arrayInput.length; i++) {
+        lista += `${i + 1}. ${arrayInput[i]} \n`
+    }
+
+    textResp.innerHTML = lista;
     form.pacienteInput.value = "";
-
-    console.log(arrayInput)
-
 })
 
 
+form.btUrgencia.addEventListener("click", () => {
+    let inputName = form.pacienteInput.value;
+    arrayInput.unshift(inputName);
+    let lista = "";
 
+    arrayInput.forEach((nome, index) => {
+        lista += `${index + 1}. ${nome}\n`
+    })
+
+    textResp.innerHTML = lista;
+    form.pacienteInput.value = "";
+})
+
+form.btAtender.addEventListener("click", () => {
+    let inputName = form.pacienteInput.value;
+    let arraySplice = arrayInput.splice(0, 1)
+    let lista = "";
+
+    arrayInput.forEach((nome, index) => {
+        lista += `${index + 1}. ${nome}`
+    })
+
+    textAtendimento.innerHTML = `Em atendimento: ${arraySplice}`
+    textResp.innerHTML = lista;
+    form.pacienteInput.value = "";
+})
